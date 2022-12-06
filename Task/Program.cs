d847d0d623403903a55edc7b0ab12fcde3fbf2f3
Sergey <cpartak.89@mail.ru>
2022-12-06 21:51:09 +0300

    Add test number

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,34 +1,38 @@
 ﻿Console.Write("Введите нужное количество строк в массиве: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-string[] sourceArray = new string[number];
-for (int i = 0; i < sourceArray.Length; i++)
+if (number > 0)
 {
-    Console.Write($"Введите {i + 1}-ю строку: ");
-    sourceArray[i] = Console.ReadLine();
-}
-
-void ArrayThreeSymbols(string[] array)
-{
-    Console.Write("-> [");
-    for (int i = 0; i < array.Length; i++)
+    string[] sourceArray = new string[number];
+    for (int i = 0; i < sourceArray.Length; i++)
     {
-        if (array[i].Length <= 3)
-        {
-            Console.Write(array[i] + ", ");
-        }
+        Console.Write($"Введите {i + 1}-ю строку: ");
+        sourceArray[i] = Console.ReadLine();
     }
-    Console.WriteLine("]");
-}
 
-Console.WriteLine();
-Console.WriteLine("Результат:");
-Console.Write("[");
-for (int i = 0; i < sourceArray.Length; i++)
-{
-    if (i < sourceArray.Length - 1) Console.Write(sourceArray[i] + "," + " ");
-    else Console.Write(sourceArray[i] + "] ");
-}
+    void ArrayThreeSymbols(string[] array)
+    {
+        Console.Write("-> [");
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= 3)
+            {
+                Console.Write(array[i] + ", ");
+            }
+        }
+        Console.WriteLine("]");
+    }
 
-ArrayThreeSymbols(sourceArray);
+    Console.WriteLine();
+    Console.WriteLine("Результат:");
+    Console.Write("[");
+    for (int i = 0; i < sourceArray.Length; i++)
+    {
+        if (i < sourceArray.Length - 1) Console.Write(sourceArray[i] + "," + " ");
+        else Console.Write(sourceArray[i] + "] ");
+    }
+
+    ArrayThreeSymbols(sourceArray);
+}
+else Console.WriteLine("Введено некорректное значение!");
 
